83fdc0959c82d97f9925fed16504b8b2b140c378
mykey <mykey@nixos.local>
2026-05-14 18:26:19 +0800

    fix: Twitter Color Emoji (Apple emoji unavailable in nixpkgs)

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -22,8 +22,8 @@
           name = "JetBrainsMono Nerd Font";
         };
         emoji = {
-          package = pkgs.apple-color-emoji;
-          name = "Apple Color Emoji";
+          package = pkgs.twitter-color-emoji;
+          name = "Twitter Color Emoji";
         };
       };
     };

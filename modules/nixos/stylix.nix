f39143e5f0e0e4e5312c12e129230ea5071094e1
mykey <mykey@nixos.local>
2026-05-14 18:29:06 +0800

    feat: real Apple Color Emoji from samuelngs/apple-emoji-ttf

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -22,8 +22,20 @@
           name = "JetBrainsMono Nerd Font";
         };
         emoji = {
-          package = pkgs.twitter-color-emoji;
-          name = "Twitter Color Emoji";
+          package = pkgs.stdenv.mkDerivation {
+            pname = "apple-color-emoji";
+            version = "26";
+            src = pkgs.fetchurl {
+              url = "https://github.com/samuelngs/apple-emoji-ttf/releases/download/macos-26-20260219-2aa12422/AppleColorEmoji-Linux.ttf";
+              sha256 = "sha256-U1oEOvBHBtJEcQWeZHRb/IDWYXraLuo0NdxWINwPUxg=";
+            };
+            dontUnpack = true;
+            installPhase = ''
+              mkdir -p $out/share/fonts/truetype
+              cp $src $out/share/fonts/truetype/AppleColorEmoji.ttf
+            '';
+          };
+          name = "Apple Color Emoji";
         };
       };
     };

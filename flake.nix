d30a9947a654fd608db276af5305e9c33b5a7b13
mykeyy <mykey@mykeyy.tech>
2026-05-25 19:19:30 +0800

    flake: add opencode input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    opencode = {
+      url = "github:anomalyco/opencode/dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
